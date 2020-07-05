0d7f09d9d6b799cb36154e96f6af4d4d70cdce3b
Peter Salz <psalz.app.dev@web.de>
2020-07-05 20:49:06 +0200

    Added Astronaut model and loading code

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Peter Salz on 05.07.20.
+//  Copyright © 2020 Peter Salz App Development. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    
+    let id: String
+    let name: String
+    let description: String
+}
